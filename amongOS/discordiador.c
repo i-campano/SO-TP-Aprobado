c32d3b44c006b3f12fca8b9635940b2bbd7789f0
Ignacio Campano <icampano@frba.utn.edu.ar>
2021-04-29 08:29:40 -0300

    estructura inicial:discordiador

---
+++ amongOS/discordiador.c
@@ -0,0 +1,70 @@
+#include "discordiador.h"
+
+int main(void) {
+	// PARTE 2
+	t_log* logger = iniciar_logger();
+	log_info(logger, "Soy el Discordiador");
+
+	t_config* config = leer_config();
+	char* valor = config_get_string_value(config, "CLAVE");
+	log_info(logger, valor);
+
+	char* ip = config_get_string_value(config, "IP");
+	log_info(logger, ip);
+	leer_consola(logger);
+
+	// PARTE 3
+	int conexion = crear_conexion(
+		config_get_string_value(config, "IP"),
+		config_get_string_value(config, "PUERTO")
+	);
+
+
+	enviar_mensaje(valor, conexion);
+
+	t_paquete* paquete = armar_paquete();
+
+	enviar_paquete(paquete, conexion);
+
+	// Viejo terminar programa
+	eliminar_paquete(paquete);
+	log_destroy(logger);
+	config_destroy(config);
+	close(conexion);
+}
+
+t_log* iniciar_logger() {
+	return log_create("discordiador.log", "DISCORDIADOR", 1, LOG_LEVEL_INFO);
+}
+
+t_config* leer_config() {
+	return config_create("discordiador.config");
+}
+
+void leer_consola(t_log* logger) {
+	char* leido = readline(">");
+
+	while(strncmp(leido, "", 1) != 0) {
+		log_info(logger, leido);
+		free(leido);
+		leido = readline(">");
+	}
+
+	free(leido);
+}
+
+t_paquete* armar_paquete() {
+	t_paquete* paquete = crear_paquete();
+
+	char* leido = readline(">");
+
+	while(strncmp(leido, "", 1) != 0) {
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		free(leido);
+		leido = readline(">");
+	}
+
+	free(leido);
+
+	return paquete;
+}
---
+++ amongOS/discordiador.h
@@ -0,0 +1,24 @@
+/*
+ * Cliente.h
+ *
+ *  Created on: 28 feb. 2019
+ *      Author: utnso
+ */
+
+#ifndef DISCORDIADOR_H_
+#define DISCORDIADOR_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+
+#include "utils.h"
+
+t_log* iniciar_logger(void);
+t_config* leer_config(void);
+void leer_consola(t_log* logger);
+t_paquete* armar_paquete();
+#endif /* DISCORDIADOR_H_ */
